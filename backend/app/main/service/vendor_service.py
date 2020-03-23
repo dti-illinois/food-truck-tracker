13651da78e479ec139d803f4fe252f151689b2b6
zifan feng <zifanfeng@gmail.com>
2020-03-23 13:57:31 -0500

    backend impl is_open field

--- backend/app/main/service/vendor_service.py
+++ backend/app/main/service/vendor_service.py
@@ -1,10 +1,24 @@
 from ..model.vendor_model import Vendor
+from datetime import date, datetime, timezone
 from flask import json 
 from bson import json_util  
 
+ISO_FORMAT = 'YYYY-MM-DDTHH:MM:SS'
+
 def get_all_vendors():
-    ret = json_util.loads(Vendor.objects().to_json())
-    return ret
+	ret = json_util.loads(Vendor.objects().to_json())
+	return list(map(add_vendor_info, ret))
 
 def get_vendor_by_username(username):
-    return json_util.loads(Vendor.objects.get(username=username).to_json())
+	ret = json_util.loads(Vendor.objects.get(username=username).to_json())
+	return add_vendor_info(ret)
+
+# The function takes in vendor as a json object 
+def add_vendor_info(vendor):
+	start, end = vendor['schedule']['start'].time(), vendor['schedule']['end'].time()
+	now = datetime.now(timezone.utc).time()
+	if now >= start and now <= end:
+		vendor['is_open'] = True
+	else:
+		vendor['is_open'] = False
+	return vendor 
